615d7c89f716535d685192cd9576accf8ad81584
Josephine Walker <jw12513@bigblue3.cvos.cluster>
2013-06-18 09:18:28 +0100

    corrected the way to submit batch jobs

--- mgmt-submit.sh
+++ mgmt-submit.sh
@@ -2,15 +2,15 @@
 
 #PBS -N test
 #PBS -j oe
-#PBS -J 1-10
+#PBS -t 1-10
 
 set -e
 
 if [ -n "${1}" ]; then
   echo "${1}"
-  PBS_ARRAY_INDEX=${1}
+  PBS_ARRAYID=${1}
 fi
 
-i=${PBS_ARRAY_INDEX}
+i=${PBS_ARRAYID}
 
 R --no-save --args ${i} < ~/mgmt-jacobian-parallel/mgmt-run.R
